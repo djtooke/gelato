1b95e638965f016d9b6e86aa5194a617ea7fca9b
Daniel Tooke <daniel.tooke@gmail.com>
2019-06-04 18:33:37 +0100

    Add method to add ingredients to gelato

--- calc/gelato.py
+++ calc/gelato.py
@@ -1,5 +1,15 @@
+from calc.ingredient import Ingredient
+
 class Gelato:
 
     def __init__(self):
         self.ingredients = []
-        
+
+
+    def add_ingredient(self, *args):
+        [self._verify_ingredient(ing) for ing in args]
+        self.ingredients.extend(args)
+
+    def _verify_ingredient(self, ing):
+        if type(ing) != Ingredient:
+            raise TypeError('Attempted to add an ingredient that was not of type Ingredient: {}'.format(ing))
--- tests/test_gelato.py
+++ tests/test_gelato.py
@@ -1,6 +1,7 @@
 import unittest
 
 from calc.gelato import Gelato
+from calc.ingredient import Ingredient
 
 class TestGelato(unittest.TestCase):
 
@@ -10,6 +11,19 @@ class TestGelato(unittest.TestCase):
     def test_ingredients_list(self):
         g = Gelato()
         self.assertEqual(g.ingredients, [])
+
+    def test_add_ingredient(self):
+        g = Gelato()
+        i_1 = Ingredient(20, 20, 20, 20, 20, 'Phlogiston', 100)
+        i_2 = Ingredient(0, 0, 0, 0, 100, 'Water', 100)
+        g.add_ingredient(i_1, i_2)
+        self.assertEqual(g.ingredients, [i_1, i_2])
+
+    @unittest.skip
+    def test_raise_on_adding_non_ingredient(self):
+        g = Gelato()
+        with self.assertRaises(TypeError):
+            g.add_ingredient(None)
 
 if __name__ == '__main__':
     unittest.main()
